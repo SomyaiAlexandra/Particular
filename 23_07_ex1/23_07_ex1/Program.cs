54f142a338496d659c1c932e2b065e8609cedf08
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-07-27 15:10:23 +0300

    Finish code.

---
+++ 23_07_ex1/23_07_ex1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _23_07_ex1
+{
+    class Program
+    {
+       
+            static Random rnd = new Random();
+            static void Main(string[] args)
+            {
+                int cr = 10;
+                do
+                {
+                    int a = rnd.Next(0, 8);
+                    int b = rnd.Next(0, 8);
+                    int c = rnd.Next(0, 8);
+                    cr--;
+                    Console.WriteLine(a + " " + b + " " + c + " ");
+                    Console.ReadKey();
+
+                    if ((a == b) & (b == c))
+                    {
+                        Console.WriteLine("Cele 3 numere sunt identice");
+                        cr = cr + 10;
+                    }
+
+                    else
+                    {
+                        if ((a == b) || (b == c) || (c == a))
+                        {
+                            Console.WriteLine("Doua dintre cele 3 numere sunt identice");
+                            cr = cr + 2;
+                        }
+                        else
+                        {
+                            int t;
+
+                            if (a > b)
+                            {
+                                t = a; a = b; b = t;
+                            }
+                            if (a > c)
+                            {
+                                t = a; a = c; c = t;
+                            }
+                            if (b > c)
+                            {
+                                t = b; b = c; c = t;
+                            }
+
+                            if ((c - b == b - a) & (c - b == 1))
+                            {
+                                Console.WriteLine("Numerele sunt in progresie artimetica si au ratia 1");
+                                cr = cr + 1;
+                            }
+                            else
+                                Console.WriteLine("None");
+                        }
+                    }
+                    Console.WriteLine(cr);
+                }
+                while (cr > 0);
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ 23_07_ex2/23_07_ex2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace _23_07_ex2
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+
+            int t;
+            int a = rnd.Next(1, 10);
+            int b = rnd.Next(1, 10);
+            int c = rnd.Next(1, 10);
+            int d = rnd.Next(1, 10);
+            int e = rnd.Next(1, 10);
+
+            Console.WriteLine("Numarul este " + a + " " + b + " " + c + " " + d + " " + e + " ");
+            Console.ReadKey();
+
+            if (a > b)
+            {
+                t = a; a = b; b = t;
+            }
+            if (a > c)
+            {
+                t = a; a = c; c = t;
+            }
+            if (a > d)
+            {
+                t = a; a = d; d = t;
+            }
+            if (a > e)
+            {
+                t = a; a = e; e = t;
+            }
+            if (b > c)
+            {
+                t = b; b = c; c = t;
+            }
+            if (b > d)
+            {
+                t = b; b = d; d = t;
+            }
+            if (b > e)
+            {
+                t = b; b = e; e = t;
+            }
+            if (c > d)
+            {
+                t = c; c = d; d = t;
+            }
+            if (c > e)
+            {
+                t = c; c = e; e = t;
+            }
+            if (d > e)
+            {
+                t = d; d = e; e = t;
+            }
+
+            Console.WriteLine("Numarul aranjat este " + a + " " + b + " " + c + " " + d + " " + e + " ");
+
+            if ((a == b) && (a == c) && (a == d) && (a == e))
+                Console.WriteLine("Toate cifrele sunt identice");
+            else
+
+                if (((a == b) && (a == c) && (a == d)) || ((b == c) && (b == d) && (b == e)))
+            {
+                Console.WriteLine("Sunt identice 4 cifre");
+            }
+            if (((a == b) && (a == c) && (d == e)) || ((a == b) && (c == d) && (c == e)))
+            {
+                Console.WriteLine("Sunt identice 3 cu 2");
+            }
+            if (((a == b) && (a == c)) || ((b == c) && (b == d)) || (c == d) && (c == e))
+            {
+                Console.WriteLine("Sunt identice 3 cifre");
+            }
+            if (((a == b) && (c == d)) || ((b == c) && (d == e)) || ((a == b) & (d == e)))
+            {
+                Console.WriteLine("Sunt identice 2 cate 2");
+            }
+            if ((a == b) || (b == c) || (c == d) || (d == e))
+            {
+                Console.WriteLine("Sunt identice 2 cifre");
+            }
+
+
+            else
+                Console.WriteLine("Numerele nu sunt identice");
+
+            Console.ReadKey();
+        }
+    }
+}
+  
